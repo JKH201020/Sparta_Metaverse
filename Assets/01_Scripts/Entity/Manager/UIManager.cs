e39990d92ca69b2da25b1e92c909caddd2a8180d
JKH201020 <jack0517@naver.com>
2026-03-25 23:22:41 +0900

    feat: UIManager에 로딩UI, 페이드UI 오브젝트 연결

--- Assets/01_Scripts/Entity/Manager/UIManager.cs
+++ Assets/01_Scripts/Entity/Manager/UIManager.cs
@@ -1,7 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class UIManager : Singleton<UIManager>
 {
+    [SerializeField] private GameObject _loadingPanel;
+    [SerializeField] private Image _fadePanel;
+
+    private const String LoadingPanelString = "Canvas/LoadingPanel";
+    private const String FadePanelString = "Canvas/FadePanel";
+
+    private void Reset()
+    {
+        _loadingPanel = transform.Find(LoadingPanelString).gameObject;
+        _fadePanel = transform.Find(FadePanelString).GetComponent<Image>();
+    }
+
+    #region 패널 온오프
+
+    /// <summary>
+    /// 로딩 패널 활성화
+    /// </summary>
+    public void OnLoadingPanel() 
+    {
+        _loadingPanel.SetActive(true);
+    }
+
+    /// <summary>
+    /// 로딩 패널 비활성화
+    /// </summary>
+    public void OffLoadingPanel() 
+    {
+        _loadingPanel.SetActive(false);
+    }
+
+    #endregion
+
     protected override void Awake()
     {
 
+    }
+
+    private async Task OnLoading() // 로딩화면
+    {
+        
     }
 }
